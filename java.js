a8f26cb445966b7930105bc97e39e31d82ae47b6
Alan Fernandes <alanf3878@outlook.com>
2021-05-29 10:16:52 -0300

    trabalho4-

---
+++ java.js
@@ -0,0 +1,37 @@
+function oi (){
+    console.log("Óla")
+}
+
+oi( )
+
+
+function  nome (myname){
+ 
+    console.log(`meu nome é ${myname}`)
+
+}
+
+nome("Alan")
+
+
+function soueu (nome,idade,musica){
+    console.log(`meu nome é ${nome}, tenho ${idade}, e esuto ${musica}`)
+
+}
+
+soueu("ALAN", 24, "RAP")
+
+
+function favorito(filme,musica){
+    console.log(`meu filme favorito é ${filme}, e minha musica favorita é ${musica}`)
+
+}
+
+favorito ("John Wick","Bk-bloco7")
+
+
+function triplo(vezes){
+    return vezes*3 
+}
+
+console.log(triplo(5))
