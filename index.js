8b6983e83070028b63b08dd081c3209d8fa837e7
Alan Fernandes <alanff3878@outlook.com>
2021-08-31 19:16:55 -0300

    arrumei as funções

---
+++ index.js
@@ -0,0 +1,24 @@
+function teste (){
+    console.log("Óla")
+  }
+  teste( )
+  
+  function  nome (meunome){
+    console.log(`meu nome é ${meunome}`)
+  }
+  nome("Alan")
+  
+  function pessoa (nome,idade,musica){
+    console.log(`meu nome é ${nome}, tenho ${idade}, e esuto ${musica}`)
+  }
+  pessoa("ALAN", 24, "RAP")
+  
+  function favorito(filme,musica){
+    console.log(`meu filme favorito é ${filme}, e minha musica favorita é ${musica}`)
+  }
+  favorito ("John Wick","Bk-bloco7")
+  
+  function conta(vezes){
+    return vezes*3 
+  }
+  console.log(conta(5))
